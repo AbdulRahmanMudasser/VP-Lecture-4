6e169da77d045406a08438e044e34faf091fdea8
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-19 12:29:48 +0500

    Add project files.

---
+++ VP Lecture 4/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VP_Lecture_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+
+            form2.Show();
+
+            this.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int firstNumber = Convert.ToInt32(textBox1.Text);
+            int secondNumber = Convert.ToInt32(textBox2.Text);
+            int sum = firstNumber + secondNumber;
+
+            textBox3.Text = sum.ToString();
+        }
+    }
+}
